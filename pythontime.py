8f5037d79cf65e41662d7c8a22a7c06fd8e21213
grapefruit95 <rocco.perciavalleiii@gmail.com>
2020-06-22 23:37:29 -0500

    created time variable strings in while loop

--- pythontime.py
+++ pythontime.py
@@ -1,2 +1,10 @@
 import datetime
-print(datetime.datetime.now())
+import time
+
+while True:
+    currentTime = (datetime.datetime.now())
+    day = str(currentTime.day)
+    month = str(currentTime.month)
+    hour = str(currentTime.strftime("%I"))
+    minute = str(currentTime.strftime("%M"))
+    second = str(currentTime.strftime("%S"))

5d8392a2c1c88be56e27b97b53a39ed786234198
grapefruit95 <rocco.perciavalleiii@gmail.com>
2020-06-23 10:44:28 -0500

    added openweathermap api calls. Sorts weather data by location into locationWeather dictionary

---
+++ rpiclockweather.py
@@ -0,0 +1,23 @@
+from geopy.geocoders import Nominatim
+import requests
+
+locations = ["Spring Branch,Texas", "San Antonio,Texas", "New Braunfels,Texas"]
+locationLatLongs = {}
+locationWeather = {}
+for location in locations:
+    locationLatLongs[location] = [Nominatim(user_agent="rpiclockweather").geocode(location, addressdetails=True).raw['lat'], Nominatim(user_agent="rpiclockweather").geocode(location, addressdetails=True).raw['lon']]
+
+    url = "https://community-open-weather-map.p.rapidapi.com/weather"
+
+    querystring = {"callback":"test","id":"2172797","units":"%22metric%22 or %22imperial%22","mode":"xml%2C html","q":location}
+
+    headers = {
+        'x-rapidapi-host': "community-open-weather-map.p.rapidapi.com",
+        'x-rapidapi-key': "42d1462079mshead0e3cee7cec9fp130467jsnadc27d29ba00"
+        }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+
+    locationWeather[location] = response.text
+
+print(locationWeather)

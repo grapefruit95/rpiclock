bf6ad60c73a5eac6aec5c6e2456a38ed1ac9c39f
grapefruit95 <rocco.perciavalleiii@gmail.com>
2020-06-23 15:00:27 -0500

    combined clock and weather scripts into multithreaded single script

--- rpiclockweather.py
+++ rpiclockweather.py
@@ -1,15 +1,12 @@
+from threading import Thread
 import requests
 import pyowm
 import datetime
 import time
 import geocoder
-
-
-owm = pyowm.OWM("a51b8b1104bc88753d99c08008b0717a")
-mgr = owm.weather_manager()
-
-
-def updateWeather():
+def updateWeather(g):
+    owm = pyowm.OWM("a51b8b1104bc88753d99c08008b0717a")
+    mgr = owm.weather_manager()
     try:
         observation = mgr.weather_at_coords(g.latlng[0],g.latlng[1])
     except:
@@ -17,7 +14,9 @@ def updateWeather():
     temp_data = observation.to_dict()['weather']['temperature']
     return str(round(pyowm.utils.measurables.kelvin_to_fahrenheit(temp_data['temp'])))
 
-def updateForecast():
+def updateForecast(g):
+    owm = pyowm.OWM("a51b8b1104bc88753d99c08008b0717a")
+    mgr = owm.weather_manager()
     try:
         forecast = mgr.forecast_at_coords(g.latlng[0],g.latlng[1], "3h", limit=8)
     except:
@@ -33,27 +32,41 @@ def updateForecast():
             minTemp = x['temperature']['temp_min']
     return [str(round(pyowm.utils.measurables.kelvin_to_fahrenheit(maxTemp))), str(round(pyowm.utils.measurables.kelvin_to_fahrenheit(minTemp)))]
 
-try:
-    g = geocoder.ip('me')
-except:
-    g = "No Connection"
-currentTemp = updateWeather()
-maxMinTemp = updateForecast()
-outputText = (currentTemp+u'\N{DEGREE SIGN}'+"        "+maxMinTemp[0]+'/'+maxMinTemp[1]+"                ")[0:16]
+def clockLine():
+    userDayDelta = 0
+    userHourDelta = 0
+    while True:
+        currentTime = (datetime.datetime.now()+datetime.timedelta(days=userDayDelta, hours=userHourDelta))
 
-while True: 
+        time_str = datetime.datetime.strftime(currentTime, "%I:%M%p    %m/%d")
+
+        print(time_str)
+        time.sleep(2)
+
+def weatherLine():
     try:
         g = geocoder.ip('me')
     except:
         g = "No Connection"
-    print(outputText)
-    currentTemp = updateWeather()
-    if currentTemp == "API Request Fail":
-        outputText = "API Request Fail"
-    else:
-        outputText = (currentTemp+u'\N{DEGREE SIGN}'+"        "+maxMinTemp[0]+'/'+maxMinTemp[1]+"                ")[0:16]
-    if (datetime.datetime.now().hour == 0 and datetime.datetime.now().minute in range(0,5)) or (maxMinTemp[0]+'/'+maxMinTemp[1] == "N/A"):
-        maxMinTemp = updateForecast()
+    currentTemp = updateWeather(g)
+    maxMinTemp = updateForecast(g)
+    outputText = (currentTemp+u'\N{DEGREE SIGN}'+"        "+maxMinTemp[0]+'/'+maxMinTemp[1]+"                ")[0:16]
 
-    time.sleep(10)
+    while True: 
+        try:
+            g = geocoder.ip('me')
+        except:
+            g = "No Connection"
+        print(outputText)
+        currentTemp = updateWeather(g)
+        if currentTemp == "API Request Fail":
+            outputText = "API Request Fail"
+        else:
+            outputText = (currentTemp+u'\N{DEGREE SIGN}'+"        "+maxMinTemp[0]+'/'+maxMinTemp[1]+"                ")[0:16]
+        if (datetime.datetime.now().hour == 0 and datetime.datetime.now().minute in range(0,5)) or (maxMinTemp[0]+'/'+maxMinTemp[1] == "N/A"):
+            maxMinTemp = updateForecast(g)
 
+        time.sleep(2)
+
+Thread(target=clockLine).start()
+Thread(target=weatherLine).start()

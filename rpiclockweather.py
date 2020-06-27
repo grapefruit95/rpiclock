741e5c6901a6884b41c9344fd31df63ff9852b62
grapefruit95 <rocco.perciavalleiii@gmail.com>
2020-06-27 12:58:29 -0500

    display

--- rpiclockweather.py
+++ rpiclockweather.py
@@ -79,7 +79,7 @@ def clock():
         print(time_str)
         lcd.lcd_byte(lcd.LCD_LINE_1, lcd.LCD_CMD)
         lcd.lcd_string(time_str, 2)
-        if(currentTime.hour == 0 and currentTime.minute in range(0,5)):
+        if(currentTime.minute % 2 == 0):
           weatherLine()
         time.sleep(0.1)
 

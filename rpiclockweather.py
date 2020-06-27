b85e715e5efb81ffda721b03433310a8e5cdc94a
grapefruit95 <rocco.perciavalleiii@gmail.com>
2020-06-27 12:09:40 -0500

    display :

--- rpiclockweather.py
+++ rpiclockweather.py
@@ -9,109 +9,18 @@ userDayDelta = 0
 userHourDelta = 0
 currentTime = (datetime.datetime.now()+datetime.timedelta(days=userDayDelta, hours=userHourDelta))
 
-def lcd_init():
-  # Initialise display
-  lcd_byte(0x33,LCD_CMD) # 110011 Initialise
-  lcd_byte(0x32,LCD_CMD) # 110010 Initialise
-  lcd_byte(0x06,LCD_CMD) # 000110 Cursor move direction
-  lcd_byte(0x0C,LCD_CMD) # 001100 Display On,Cursor Off, Blink Off
-  lcd_byte(0x28,LCD_CMD) # 101000 Data length, number of lines, font size
-  lcd_byte(0x01,LCD_CMD) # 000001 Clear display
-  time.sleep(E_DELAY)
- 
-def lcd_byte(bits, mode):
-  # Send byte to data pins
-  # bits = data
-  # mode = True  for character
-  #        False for command
- 
-  GPIO.output(LCD_RS, mode) # RS
-  # High bits
-  GPIO.output(LCD_D4, False)
-  GPIO.output(LCD_D5, False)
-  GPIO.output(LCD_D6, False)
-  GPIO.output(LCD_D7, False)
-  if int(bits&int(0x10))==0x10:
-    GPIO.output(LCD_D4, True)
-  if int(bits&int(0x20))==0x20:
-    GPIO.output(LCD_D5, True)
-  if int(bits&int(0x40))==0x40:
-    GPIO.output(LCD_D6, True)
-  if int(bits&int(0x80))==0x80:
-    GPIO.output(LCD_D7, True)
- 
-  # Low bits
-  GPIO.output(LCD_D4, False)
-  GPIO.output(LCD_D5, False)
-  GPIO.output(LCD_D6, False)
-  GPIO.output(LCD_D7, False)
-  if int(bits&int(0x01))==0x01:
-    GPIO.output(LCD_D4, True)
-  if int(bits&int(0x02))==0x02:
-    GPIO.output(LCD_D5, True)
-  if int(bits&int(0x04))==0x04:
-    GPIO.output(LCD_D6, True)
-  if int(bits&int(0x08))==0x08:
-    GPIO.output(LCD_D7, True)
- 
-def lcd_toggle_enable():
-  # Toggle enable
-  time.sleep(E_DELAY)
-  GPIO.output(LCD_E, True)
-  time.sleep(E_PULSE)
-  GPIO.output(LCD_E, False)
-  time.sleep(E_DELAY)
- 
-def lcd_string(message,line):
-  # Send string to display
-  message = message.ljust(LCD_WIDTH," ")
-  
-  lcd_byte(line, LCD_CMD)
- 
-  for i in range(LCD_WIDTH):
-    lcd_byte(ord(message[i]),LCD_CHR)
-
-# Toggle 'Enable' pin
-# Define GPIO to LCD mapping
-LCD_RS = 25
-LCD_E  = 24
-LCD_D4 = 23
-LCD_D5 = 17
-LCD_D6 = 18
-LCD_D7 = 22
 HOURPLUS = 5
 HOURMINUS = 6
 DAYPLUS = 27
 DAYMINUS = 13
- 
-# Define some device constants
-LCD_WIDTH = 16    # Maximum characters per line
-LCD_CHR = True
-LCD_CMD = False
- 
-LCD_LINE_1 = 0x80 # LCD RAM address for the 1st line
-LCD_LINE_2 = 0xC0 # LCD RAM address for the 2nd line
- 
-# Timing constants
-E_PULSE = 0.001
-E_DELAY = 0.001
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)       # Use BCM GPIO numbers
-GPIO.setup(LCD_E, GPIO.OUT)  # E
-GPIO.setup(LCD_RS, GPIO.OUT) # RS
-GPIO.setup(LCD_D4, GPIO.OUT) # DB4
-GPIO.setup(LCD_D5, GPIO.OUT) # DB5
-GPIO.setup(LCD_D6, GPIO.OUT) # DB6
-GPIO.setup(LCD_D7, GPIO.OUT) # DB7
+
 GPIO.setup(HOURPLUS, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(HOURMINUS, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(DAYPLUS, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(DAYMINUS, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-lcd_toggle_enable()
-
-lcd_init()
 
 location = "Spring Branch, Texas"
 outputText = ""

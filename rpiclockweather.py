6689afa94024f4cfbf84bddbd24a1ca26165847d
grapefruit95 <rocco.perciavalleiii@gmail.com>
2020-06-27 12:15:04 -0500

    display :

--- rpiclockweather.py
+++ rpiclockweather.py
@@ -4,7 +4,17 @@ import pyowm
 import datetime
 import time
 import RPi.GPIO as GPIO
-import time
+import sys
+sys.path.append('/home/pi/lcd')
+import lcd
+
+lcd.lcd_init()
+lcd.lcd_byte(lcd.LCD_LINE_1, lcd.LCD_CMD)
+lcd.lcd_string("Raspberry Pi", 2)
+lcd.lcd_byte(lcd.LCD_LINE_2, lcd.LCD_CMD)
+lcd.lcd_string("Model B+", 2)
+lcd.GPIO.cleanup()
+
 userDayDelta = 0
 userHourDelta = 0
 currentTime = (datetime.datetime.now()+datetime.timedelta(days=userDayDelta, hours=userHourDelta))
